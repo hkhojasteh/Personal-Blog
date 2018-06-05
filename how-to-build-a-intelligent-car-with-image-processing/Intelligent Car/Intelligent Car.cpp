32fbc5f08695671f59357d2d24e6416c00e5b4b8
Hadi Abdi Khojasteh <khojasteh.programmer@gmail.com>
2018-06-06 03:20:05 +0430

    blog post.txt and Intelligent Car.cpp updated Some assets added

--- how-to-build-a-intelligent-car-with-image-processing/Intelligent Car/Intelligent Car.cpp
+++ how-to-build-a-intelligent-car-with-image-processing/Intelligent Car/Intelligent Car.cpp
@@ -12,40 +12,35 @@ using namespace cv;
 using namespace std;
 
 Mat3b img;
-Mat img_gray, dst, detected_edges;
-int lowThreshold;
-int const max_lowThreshold = 100;
-int ratio = 3;
-int kernel_size = 3;
+Mat detected_edges, img_mask;
+int const max_lowThreshold = 255;
+int lowThreshold = 210, ratio = 3, kernel_size = 3;
 char* window_name = "Edge Map";
 // CannyThreshold: Trackbar callback - Canny thresholds input with a ratio 1:3
 void CannyThreshold(int, void*){
-	/// Reduce noise with a kernel 3x3
-	blur(img_gray, detected_edges, Size(3, 3));
+	// Reduce noise with a kernel 3x3
+	blur(img_mask, detected_edges, Size(3, 3));
+	
+	// Canny detector
+	Canny(detected_edges, detected_edges, lowThreshold, lowThreshold * ratio, kernel_size);
 
-	/// Canny detector
-	Canny(detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size);
-
-	/// Using Canny's output as a mask, we display our result
-	dst = Scalar::all(0);
-
-	img.copyTo(dst, detected_edges);
-	imshow(window_name, dst);
+	// Using Canny's output as a mask and display our result
+	imshow(window_name, detected_edges);
 }
 
 int main(int argc, char** argv){
 	// Load image
-	img = imread("..\\assets\\Road Sample\\Sample1.png");
+	img = imread("..\\assets\\Road Sample\\Sample10.png");
 
 	// Setup a rectangle to define your region of interest
 	//Rect roi(0, 325, img.cols, img.rows - 455); //5
-	Rect roi(0, 400, img.cols, img.rows - 480); //1 , 10
+	Rect roi(0, 420, img.cols, img.rows - 500); //1 , 10
 
 	//Crop the full image to that image contained by the rectangle myROI
 	Mat3b img_crop = img(roi);
 
 	// Convert image to gray, blur and sharpen it
-	Mat mask_hsv_yellow, mask_white, img_mask;
+	Mat img_gray, mask_hsv_yellow, mask_white;
 	cvtColor(img_crop, img_gray, CV_BGR2GRAY);
 
 	// Make target image by apply yellow and white mask
@@ -56,9 +51,9 @@ int main(int argc, char** argv){
 	bitwise_or(mask_white, mask_hsv_yellow, img_mask);
 	GaussianBlur(img_mask, img_mask, cv::Size(5, 5), 0);
 
-	dst.create(img_gray.size(), img_gray.type());
+	detected_edges.create(img_mask.size(), img_mask.type());
 	// Create a window
-	namedWindow(window_name, CV_WINDOW_AUTOSIZE);
+	namedWindow(window_name);
 	// Create a Trackbar for user to enter threshold
 	createTrackbar("Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold);
 	// Show the image
@@ -67,7 +62,7 @@ int main(int argc, char** argv){
 	// Show result
 	//imshow("Original", img);
 	//imshow("Crop", crop);
-	//imshow("Gray", img_gray);
+	imshow("Gray", img_gray);
 	//imshow("Mask", img_mask);
 
 	waitKey(0);
