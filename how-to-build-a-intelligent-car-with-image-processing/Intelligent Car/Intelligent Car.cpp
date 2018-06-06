58b07988e4998c5449be5cb82d8a592b64301a26
Hadi Abdi Khojasteh <khojasteh.programmer@gmail.com>
2018-06-06 18:15:28 +0430

    Find reference line in Intelligent Car.cpp

--- how-to-build-a-intelligent-car-with-image-processing/Intelligent Car/Intelligent Car.cpp
+++ how-to-build-a-intelligent-car-with-image-processing/Intelligent Car/Intelligent Car.cpp
@@ -18,45 +18,74 @@ bool less_left(const Vec4i& lhs, const Vec4i& rhs){
 	return lhs[0] < rhs[0];
 }
 bool less_right(const Vec4i& lhs, const Vec4i& rhs) {
-	return lhs[0] > rhs[0];
+	return lhs[0] < rhs[0];
 }
 
 // Standard Hough Line Transform
 void HoughTransform(void*) {
 	Mat img_hlines = img_crop.clone();
 	// will hold the results of the detection
-	vector<Vec4i> lines, rightls, leftls;
+	vector<Vec4f> lines, rightls, leftls;
 	// runs the actual detection
 	HoughLinesP(detected_edges, lines, 1, CV_PI / 180, 50, 30, 10);
 	// Draw the lines
 	for (size_t i = 0; i < lines.size(); i++) {
-		Vec4i l = lines[i];
+		Vec4f l = lines[i];
 		line(img_hlines, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 255, 255), 2, CV_AA);
 		
 		//Calculating the slope and group lines
 		float slope = (float)(l[3] - l[1]) / (l[2] - l[0]);
 		if (slope > 0.40) {
-			rightls.push_back(l);
 			printf("right %f\n", slope);
+			rightls.push_back(l);
 		}else if (slope < -0.40) {
-			leftls.push_back(l);
 			printf("left %f\n", slope);
+			leftls.push_back(l);
 		}
 	}
-	auto lmmx = minmax_element(leftls.begin(), leftls.end(), less_left);
-	Point left_b(get<0>(lmmx)[0][0], get<0>(lmmx)[0][1]);
-	Point left_t(get<0>(lmmx)[0][2], get<0>(lmmx)[0][3]);
-	auto rmmx = minmax_element(rightls.begin(), rightls.end(), less_right);
-	Point right_b(get<0>(rmmx)[0][0], get<0>(rmmx)[0][1]);
-	Point right_t(get<0>(rmmx)[0][2], get<0>(rmmx)[0][3]);
+	Point left_b, left_t, right_b, right_t;
+	if (leftls.size() > 0) {
+		auto lmmx = minmax_element(leftls.begin(), leftls.end(), less_left);
+		left_b = Point(get<0>(lmmx)[0][0], get<0>(lmmx)[0][1]);
+		left_t = Point(get<0>(lmmx)[0][2], get<0>(lmmx)[0][3]);
+	}
+	if (rightls.size() > 0) {
+		auto rmmx = minmax_element(rightls.begin(), rightls.end(), less_right);
+		right_t = Point(get<0>(rmmx)[0][0], get<0>(rmmx)[0][1]);
+		right_b = Point(get<0>(rmmx)[0][2], get<0>(rmmx)[0][3]);
+	}
+
+	/*vector<Point> fitPoints;
+	fitPoints.push_back(right_b);
+	fitPoints.push_back(right_t);
+	Vec4f l(0.0 ,0.0 ,0.0 ,0.0);
+	fitLine(fitPoints, l, CV_DIST_L2, 0, 0.01, 0.01);
+	line(img_hlines, Point(l[2] - m*l[0], l[2] - m*l[1]), Point(l[2] + m*l[0], l[2] + m*l[1]), (0, 255, 0));*/
 
 	Mat poly = img_hlines.clone();
-	vector<Point> vertices{ left_b, left_t, right_b, right_t };
+	vector<Point> vertices{ left_b, left_t, right_t, right_b };
 	vector<vector<Point>> pts{ vertices };
 	fillPoly(poly, pts, Scalar(58, 190, 37, 0));
-
 	addWeighted(poly, 0.50, img_hlines, 0.50, 0, img_hlines);
 
+	// Find reference line points by average top and bottom middle
+	float rx = (((right_b.x - left_b.x) / 2 + left_b.x) + ((right_t.x - left_t.x) / 2 + left_t.x)) / 2;
+	float ry = img_hlines.rows;
+	// Draw dashed reference line
+	Point p1(rx, 0);
+	Point p2(rx, ry);
+	LineIterator itl1(img_hlines, p1, p2, 8);	// get a line iterator
+	LineIterator itl2(img_hlines, Point(p1.x + 1, p1.y), Point(p2.x + 1, p2.y), 8);	// get a line iterator
+	for (int i = 0; i < itl1.count; i++, itl1++, itl2++) {
+		if (i % 5 != 0) {
+			// every 5'th pixel gets dropped, blue stipple line
+			(*itl1)[1] = (*itl2)[1] = 80;
+			(*itl1)[2] = (*itl2)[2] = 75;
+		}
+	}
+	vector<Point> trivertices{ Point(rx - 7, ry - 1), Point(rx + 7, ry - 1), Point(rx, ry - 8) };
+	vector<vector<Point>> tripts{ trivertices };
+	fillPoly(img_hlines, tripts, Scalar(103, 80, 75, 0));
 
 	imshow("detected lines", img_hlines);
 }
