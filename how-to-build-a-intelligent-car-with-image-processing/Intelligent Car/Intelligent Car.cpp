ea58ced8b074ae801435886c0123923ff5e0f145
Hadi Abdi Khojasteh <khojasteh.programmer@gmail.com>
2018-06-07 18:04:19 +0430

    video capture

--- how-to-build-a-intelligent-car-with-image-processing/Intelligent Car/Intelligent Car.cpp
+++ how-to-build-a-intelligent-car-with-image-processing/Intelligent Car/Intelligent Car.cpp
@@ -36,10 +36,10 @@ void HoughTransform(void*) {
 		//Calculating the slope and group lines
 		float slope = (float)(l[3] - l[1]) / (l[2] - l[0]);
 		if (slope > 0.40) {
-			printf("right %f\n", slope);
+			//printf("right %f\n", slope);
 			rightls.push_back(l);
 		}else if (slope < -0.40) {
-			printf("left %f\n", slope);
+			//printf("left %f\n", slope);
 			leftls.push_back(l);
 		}
 	}
@@ -49,11 +49,19 @@ void HoughTransform(void*) {
 		auto lmmx = minmax_element(leftls.begin(), leftls.end(), less_left);
 		left_b = Point(get<0>(lmmx)[0][0], get<0>(lmmx)[0][1]);
 		left_t = Point(get<0>(lmmx)[0][2], get<0>(lmmx)[0][3]);
+	}else{
+		// Set default values
+		left_b = Point(img_hlines.cols / 2, img_hlines.rows - 20);
+		left_t = Point(img_hlines.cols / 2, 20);
 	}
 	if (rightls.size() > 0) {
 		auto rmmx = minmax_element(rightls.begin(), rightls.end(), less_right);
 		right_t = Point(get<0>(rmmx)[0][0], get<0>(rmmx)[0][1]);
 		right_b = Point(get<0>(rmmx)[0][2], get<0>(rmmx)[0][3]);
+	}else{
+		// Set default values
+		right_t = Point(img_hlines.cols / 2, img_hlines.rows - 20);
+		right_b = Point(img_hlines.cols / 2, 20);
 	}
 
 	/*vector<Point> fitPoints;
@@ -92,7 +100,7 @@ void HoughTransform(void*) {
 }
 
 int const max_lowThreshold = 255;
-int lowThreshold = 240, ratio = 3, kernel_size = 3;
+int lowThreshold = 210, ratio = 3, kernel_size = 3;
 char* window_name = "Edge Map";
 // CannyThreshold: Trackbar callback - Canny thresholds input with a ratio 1:3
 void CannyThreshold(int, void*){
@@ -110,50 +118,68 @@ void CannyThreshold(int, void*){
 
 int main(int argc, char** argv){
 	// Load image
-	img = imread("..\\assets\\Road Sample\\Sample1.png");
+	//img = imread("..\\assets\\Road Sample\\Sample1.png");
 
-	// Or read data from camera
-	/*VideoCapture cap(0);     // open the default camera
-	if (!cap.isOpened())     // check if we successfully connected to camera
+	//VideoCapture cap(0);     // open the camera
+
+	VideoCapture cap("..\\assets\\2.mp4");
+	// Check if camera opened successfully
+	if (!cap.isOpened()) {
+		cout << "Error opening video stream or file" << endl;
 		return -1;
-	for (;;) {
-		Mat frame;
-		cap >> frame;     // get a new frame from camera
-		 // Our codes ...
-	}*/
+	}
 
-	// Setup a rectangle to define your region of interest
-	//Rect roi(0, 325, img.cols, img.rows - 455); //5
-	Rect roi(0, 420, img.cols, img.rows - 500); //1 , 10
+	while (1) {
+		// Capture frame-by-frame
+		cap >> img;
+		// If the frame is empty, break immediately
+		if (img.empty()) {
+			double count = cap.get(CV_CAP_PROP_FRAME_COUNT); //get the frame count
+			cap.set(CV_CAP_PROP_POS_FRAMES, 0); //Set index to last frame
+			continue;
+		}
+		// Display the resulting frame
+		//imshow("Frame", img);
+		// Press  ESC on keyboard to exit
+		char c = (char)waitKey(25);
+		if (c == 27)
+			break;
 
-	//Crop the full image to that image contained by the rectangle myROI
-	img_crop = img(roi);
+		// Setup a rectangle to define your region of interest
+		Rect roi(0, 390, img.cols, img.rows - 520); //5
+		//Rect roi(0, 420, img.cols, img.rows - 500); //1 , 10
 
-	// Convert image to gray, blur and sharpen it
-	Mat img_gray, mask_hsv_yellow, mask_white;
-	cvtColor(img_crop, img_gray, CV_BGR2GRAY);
+		//Crop the full image to that image contained by the rectangle myROI
+		img_crop = img(roi);
 
-	// Make target image by apply yellow and white mask
-	Scalar m = mean(img_gray);
-	cvtColor(img, mask_hsv_yellow, CV_BGR2HSV);
-	inRange(img_crop, Scalar(20, 85, 85), Scalar(30, 255, 255), mask_hsv_yellow);
-	inRange(img_gray, Scalar(m[0] + (255 - m[0]) / 3.5), Scalar(255), mask_white);
-	bitwise_or(mask_white, mask_hsv_yellow, img_mask);
-	GaussianBlur(img_mask, img_mask, cv::Size(5, 5), 0);
+		// Convert image to gray, blur and sharpen it
+		Mat img_gray, mask_hsv_yellow, mask_white;
+		cvtColor(img_crop, img_gray, CV_BGR2GRAY);
 
-	detected_edges.create(img_mask.size(), img_mask.type());
-	// Create a window
-	namedWindow(window_name);
-	// Create a Trackbar for user to enter threshold
-	createTrackbar("Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold);
-	// Show the image
-	CannyThreshold(0, 0);
+		// Make target image by apply yellow and white mask
+		Scalar m = mean(img_gray);
+		cvtColor(img, mask_hsv_yellow, CV_BGR2HSV);
+		inRange(img_crop, Scalar(20, 85, 85), Scalar(30, 255, 255), mask_hsv_yellow);
+		inRange(img_gray, Scalar(m[0] + (255 - m[0]) / 3.5), Scalar(255), mask_white);
+		bitwise_or(mask_white, mask_hsv_yellow, img_mask);
+		GaussianBlur(img_mask, img_mask, cv::Size(5, 5), 0);
 
-	// Show result
-	//imshow("Original", img);
-	//imshow("Crop", crop);
-	//imshow("Gray", img_gray);
-	//imshow("Mask", img_mask);
+		detected_edges.create(img_mask.size(), img_mask.type());
+		// Create a window
+		namedWindow(window_name);
+		// Create a Trackbar for user to enter threshold
+		createTrackbar("Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold);
+		// Show the image
+		CannyThreshold(0, 0);
+
+		// Show result
+		//imshow("Original", img);
+		//imshow("Crop", crop);
+		//imshow("Gray", img_gray);
+		//imshow("Mask", img_mask);
+	}
+	// When everything done, release the video capture object
+	cap.release();
 
 	waitKey(0);
 
