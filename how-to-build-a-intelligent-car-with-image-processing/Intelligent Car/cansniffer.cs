a6111d2a304372562abec9795aaf11ebc8565a4d
Hadi Abdi Khojasteh <khojasteh.programmer@gmail.com>
2018-07-01 20:46:51 +0430

    cansniffer.cs updated

--- how-to-build-a-intelligent-car-with-image-processing/Intelligent Car/cansniffer.cs
+++ how-to-build-a-intelligent-car-with-image-processing/Intelligent Car/cansniffer.cs
@@ -30,23 +30,55 @@ namespace CANReadWrite{
                 databyte = Encoding.ASCII.GetBytes(ui_rtb_dataRead.Text);
                 File.WriteAllBytes(DateTime.Now.ToString("HH:mm:ss") + ".txt", databyte);
             }
+            //CabelPort.WriteLine();
+
+
         }
 
-        private void ui_btn_write_Click(object sender, EventArgs e){
-            //CabelPort.WriteLine();
-            ui_lbl_info.Text = "+ ";
-            byte[] databyte = File.ReadAllBytes("data02.txt");
-            for (int i = 0; i < databyte.Length; i++){
-                string byted = (databyte[i]).ToString("X");
-                if (byted == "7F"){
-                    ui_rtb_dataRead.Text += "[" + (databyte[i]).ToString("X") + "]         ";
-                    ui_lbl_info.Text += "7F ";
-                }else{
-                    ui_rtb_dataRead.Text += (databyte[i]).ToString("X");
+        private void ui_btn_readfile_Click(object sender, EventArgs e){
+            List<string> frames = new List<string>(); 
+            int nof7Fs = 0;
+            byte[] databyte = File.ReadAllBytes("data01.txt");
+            for (int i = 0; i < databyte.Length; i += 2){
+                string fbyted = (databyte[i]).ToString("X2");
+                if (fbyted == "7F"){
+                    nof7Fs++;
+                    frames.Add(fbyted);
                 }
-                //ui_rtb_dataRead.Text += (BitConverter.ToInt16(databyte, i) | 0x7F).ToString("X");
-                ui_rtb_dataRead.Text += " ";
+                if (i + 1 < databyte.Length){
+                    string sbyted = (databyte[i + 1]).ToString("X2");
+                    if (sbyted == "7F"){
+                        nof7Fs++;
+                    }
+                    ui_rtb_dataRead.Text += fbyted + sbyted + " ";
+                }else{
+                    ui_rtb_dataRead.Text += fbyted + " ";
+                }
+                //Refresh form intervally
+                if (i % 200 == 0){
+                    refreshRichTextBox();
+                }
             }
+            ui_lbl_info.Text = "7F: " + nof7Fs;
+            ui_rtb_frames.Text = frames.Aggregate((i, j) => i + " " + j); ;
+            refreshRichTextBox();
+
+
+
+            //ui_rtb_dataRead.Text += (BitConverter.ToInt16(databyte, i) | 0x7F).ToString("X");
+        }
+
+        private void refreshRichTextBox(){
+            for (int i = 0; i < ui_rtb_dataRead.Text.Length; i++){
+                if (ui_rtb_dataRead.Text[i] == '7' && ui_rtb_dataRead.Text[i + 1] == 'F'){
+                    ui_rtb_dataRead.SelectionStart = i;
+                    ui_rtb_dataRead.SelectionLength = 2;
+                    ui_rtb_dataRead.SelectionColor = Color.Red;
+                    ui_rtb_dataRead.SelectionBackColor = Color.Yellow;
+                }
+            }
+            ui_rtb_dataRead.ScrollToCaret();
+            Application.DoEvents();
         }
     }
 }
