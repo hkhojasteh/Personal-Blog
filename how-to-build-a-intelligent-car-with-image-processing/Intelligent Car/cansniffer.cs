7b3440281946049c3d536d2e40b773771fa85f4a
Hadi Abdi Khojasteh <khojasteh.programmer@gmail.com>
2018-07-03 19:33:40 +0430

    read text file added to cansniffer.cs

--- how-to-build-a-intelligent-car-with-image-processing/Intelligent Car/cansniffer.cs
+++ how-to-build-a-intelligent-car-with-image-processing/Intelligent Car/cansniffer.cs
@@ -19,8 +19,9 @@ namespace CANReadWrite{
 
         private void ui_btn_read_Click(object sender, EventArgs e){
             if (serialPort == null || !serialPort.IsOpen){
-                serialPort = new SerialPort("COM3");
-                //SerialPort.Open();
+                serialPort = new SerialPort("COM7");
+
+                serialPort.Open();
             }else{
                 serialPort.Close();
             }
@@ -43,7 +44,9 @@ namespace CANReadWrite{
                 if (!isRead){
                     return;
                 }
-                serialPort.Read(data, 0, serialPort.BytesToRead);
+                int len = serialPort.BytesToRead;
+                len = len > 20 ? 20 : len;
+                serialPort.Read(data, 0, len);
                 using (var stream = new FileStream(fileName, FileMode.Append)){
                     stream.Write(data, 0, data.Length);
                 }
@@ -52,17 +55,25 @@ namespace CANReadWrite{
                     hex.AppendFormat("{0:x2}{1:x2} ", data[i], data[i + 1]);
                 }
                 ui_rtb_dataRead.Text += hex.ToString();
+                Application.DoEvents();
             }
         }
 
         List<string> frames;
         private void updateFrameData(List<byte> data){
-            string idf = (BitConverter.ToUInt16(data.ToArray(), 0) & 0X7FF0).ToString("X2");
-            string ids = (BitConverter.ToUInt32(data.ToArray(), 1) & 0X03FFFF00).ToString("X2");
-            //string len = ((BitConverter.ToUInt16(data.ToArray(), 4) & 0X1E00) >> 9).ToString();
-            string len = ((BitConverter.ToUInt16(data.ToArray(), 1) & 0X01E0) >> 5).ToString();
-            frames.Add(idf + " " + ids + "  " + len);
+            try {
+                string idf = (BitConverter.ToUInt16(data.ToArray(), 0) & 0X7FF0).ToString("X2");
+                string ids = (BitConverter.ToUInt32(data.ToArray(), 1) & 0X03FFFF00).ToString("X2");
+                //string len = ((BitConverter.ToUInt16(data.ToArray(), 4) & 0X1E00) >> 9).ToString();
+                string len = ((BitConverter.ToUInt16(data.ToArray(), 1) & 0X01E0) >> 5).ToString();
 
+
+                StringBuilder formatdata = new StringBuilder(data.Count * 2);
+                for (int i = 0; i < data.Count; i += 2){
+                    formatdata.AppendFormat("{0:x2}{1:x2} ", data[i], data[i + 1]);
+                }
+                frames.Add(idf + " " + ids + "  " + len + " " );
+            }catch{ };
             /*if (!frames.Exists(s => s == )){
                 frames.Add();
             }*/
@@ -71,7 +82,7 @@ namespace CANReadWrite{
         private void ui_btn_readfile_Click(object sender, EventArgs e){
             frames = new List<string>();
             int nof7Fs = 0;
-            byte[] databyte = File.ReadAllBytes("data01.txt");
+            byte[] databyte = File.ReadAllBytes("222809.txt");
             List<byte> realdata = new List<byte>();
             for (int i = 0, charCount = 0; i < databyte.Length; i++){
                 string byted = (databyte[i]).ToString("X2");
@@ -88,13 +99,13 @@ namespace CANReadWrite{
                         charCount++;
                     }
                 }
-                if (charCount == 1){
+                if (charCount == 2){
                     AppendText(this.ui_rtb_dataRead, Color.Black, Color.White, " ");
                     charCount = 0;
                 }
             }
             ui_lbl_info.Text = "7F: " + nof7Fs;
-            ui_rtb_frames.Text = frames.Aggregate((i, j) => i + "\n" + j); ;
+            ui_rtb_frames.Text = frames.Aggregate((i, j) => i + "\n" + j);
 
             //ui_rtb_dataRead.Text += (BitConverter.ToInt16(databyte, i) | 0x7F).ToString("X");
         }
@@ -111,6 +122,35 @@ namespace CANReadWrite{
                 // could set box.SelectionBackColor, box.SelectionFont too.
             }
             box.SelectionLength = 0; // clear
+        }
+
+        private void button1_Click(object sender, EventArgs e){
+            frames = new List<string>();
+            int nof7Fs = 0;
+            string databyte = File.ReadAllText("p5.txt");
+            List<byte> realdata = new List<byte>();
+            for (int i = 0, charCount = 0; i < databyte.Length - 1; i += 2){
+                string byted = databyte[i].ToString() + databyte[i + 1].ToString();
+                if (byted != "3F"){
+                    realdata.Add(Encoding.ASCII.GetBytes(byted)[0]);
+                    if (byted == "7f"){
+                        updateFrameData(realdata);
+                        nof7Fs++;
+                        realdata.Clear();
+                        AppendText(this.ui_rtb_dataRead, Color.Red, Color.Yellow, byted);
+                        charCount++;
+                    }else{
+                        AppendText(this.ui_rtb_dataRead, Color.Black, Color.White, byted);
+                        charCount++;
+                    }
+                }
+                if (charCount == 2){
+                    AppendText(this.ui_rtb_dataRead, Color.Black, Color.White, " ");
+                    charCount = 0;
+                }
+            }
+            ui_lbl_info.Text = "7F: " + nof7Fs;
+            ui_rtb_frames.Text = frames.Aggregate((i, j) => i + "\n" + j);
         }
     }
 }
